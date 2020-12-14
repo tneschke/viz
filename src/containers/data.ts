58cdf2ae0e0abbeb83da67a15d7bd7f2a44ad9a4
Teague Neschke <tneschke@gmail.com>
2020-12-14 10:11:06 -0600

    final

---
+++ src/containers/data.ts
@@ -0,0 +1,188 @@
+export const data = {
+  name: "Trolls",
+  children: [
+    {
+      name: "#",
+      children: [
+        { name: "other", value: 39139 },
+        { name: "blacklivesmatter", value: 1020 },
+        { name: "blacktwitter", value: 302 },
+        { name: "staywoke", value: 243 },
+        { name: "policebrutality", value: 79 },
+      ],
+    },
+    {
+      name: "@",
+      children: [
+        { name: "other", value: 16138 },
+        { name: "realdonaldtrump", value: 1559 },
+        { name: "potus", value: 244 },
+        { name: "cnn", value: 126 },
+        { name: "foxnews", value: 82 },
+      ],
+    },
+    {
+      name: "trump",
+      children: [
+        { name: "other", value: 9049 },
+        { name: "administration", value: 449 },
+        { name: "rally", value: 312 },
+        { name: "supporters", value: 114 },
+        { name: "voters", value: 64 },
+      ],
+    },
+    {
+      name: "police",
+      children: [
+        { name: "other", value: 5192 },
+        { name: "officers", value: 610 },
+        { name: "brutality", value: 339 },
+        { name: "shooting", value: 232 },
+      ],
+    },
+    {
+      name: "black",
+      children: [
+        { name: "other", value: 4249 },
+        { name: "people", value: 312 },
+        { name: "men", value: 139 },
+        { name: "women", value: 122 },
+      ],
+    },
+    {
+      name: "people",
+      children: [
+        { name: "other", value: 3531 },
+        { name: "are", value: 323 },
+        { name: "should", value: 234 },
+        { name: "who", value: 43 },
+      ],
+    },
+    {
+      name: "just",
+      children: [
+        { name: "other", value: 2124 },
+        { name: "because", value: 112 },
+        { name: "like", value: 64 },
+        { name: "me", value: 13 },
+      ],
+    },
+    {
+      name: "about",
+      children: [
+        { name: "other", value: 2008 },
+        { name: "to", value: 192 },
+        { name: "how", value: 44 },
+        { name: "being", value: 25 },
+      ],
+    },
+    {
+      name: "all",
+      children: [
+        { name: "other", value: 1884 },
+        { name: "you", value: 73 },
+        { name: "his", value: 34 },
+        { name: "their", value: 25 },
+      ],
+    },
+    {
+      name: "who",
+      children: [
+        { name: "other", value: 1708 },
+        { name: "would", value: 94 },
+        { name: "should", value: 62 },
+        { name: "thinks", value: 22 },
+      ],
+    },
+    {
+      name: "news",
+      children: [
+        { name: "other", value: 1713 },
+        { name: "from", value: 55 },
+        { name: "article", value: 34 },
+        { name: "story", value: 19 },
+      ],
+    },
+    {
+      name: "like",
+      children: [
+        { name: "other", value: 1624 },
+        { name: "them", value: 51 },
+        { name: "if", value: 20 },
+        { name: "you", value: 15 },
+      ],
+    },
+    {
+      name: "can",
+      children: [
+        { name: "other", value: 1649 },
+        { name: "you", value: 44 },
+        { name: "anyone", value: 42 },
+        { name: "be", value: 29 },
+      ],
+    },
+    {
+      name: "white",
+      children: [
+        { name: "other", value: 1143 },
+        { name: "house", value: 249 },
+        { name: "people", value: 123 },
+        { name: "supremacy", value: 17 },
+      ],
+    },
+    {
+      name: "world",
+      children: [
+        { name: "other", value: 838 },
+        { name: "works", value: 29 },
+        { name: "has", value: 23 },
+        { name: "according", value: 17 },
+      ],
+    },
+    {
+      name: "said",
+      children: [
+        { name: "other", value: 802 },
+        { name: "they", value: 29 },
+        { name: "he", value: 14 },
+        { name: "she", value: 9 },
+      ],
+    },
+    {
+      name: "why",
+      children: [
+        { name: "other", value: 642 },
+        { name: "they", value: 61 },
+        { name: "are", value: 53 },
+        { name: "do", value: 16 },
+      ],
+    },
+    {
+      name: "hate",
+      children: [
+        { name: "other", value: 589 },
+        { name: "all", value: 80 },
+        { name: "group", value: 30 },
+        { name: "us", value: 12 },
+      ],
+    },
+    {
+      name: "cnn",
+      children: [
+        { name: "other", value: 591 },
+        { name: "will", value: 25 },
+        { name: "gives", value: 24 },
+        { name: "does", value: 9 },
+      ],
+    },
+    {
+      name: "media",
+      children: [
+        { name: "other", value: 548 },
+        { name: "blames", value: 20 },
+        { name: "wrong", value: 12 },
+        { name: "won", value: 7 },
+      ],
+    },
+  ],
+};
